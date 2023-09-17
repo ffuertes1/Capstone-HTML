8621e9737cee52db17418a74a112dbdb928e2058
Faustino R Fuertes <ffuertes25@gmail.com>
2023-09-16 22:13:08 -0400

    Deleted

--- rid.py
+++
@@ -1,7 +0,0 @@
-FileName = "fil.txt"  # Corrected the filename
-data = open(FileName, "r").readlines()
-data.sort()
-
-for i in range(len(data)):
-    print(data[i])  # Corrected the print statement
-
